eb77b89fc4843a17c2454eae55c5ce883478e84e
andriarif <55911915+andriarif@users.noreply.github.com>
2026-05-26 15:19:33 +0700

    Add API handler for fetching lottery rounds

---
+++ api/round.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+
+  const response = await fetch(
+    "https://didihub.com/api/main/lottery/rounds?page=1&count=1&type=3",
+    {
+      headers: {
+        user_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Nzk4NjMyOTAzMjYsInBheWxvYWQiOiJJL2NTdW5DR25mcFR3MkNoay9YeVJjOEpKV2xmRGlOd2Q0T0JTOWRYaklWODZVMCs4cmFZaVZqemlWM0Nxc1EyIn0.h-BkmTlY4H-QwuvjcHc9vgjXT1rwhHF25qm1nkMdaVY"
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.status(200).json(data);
+
+}
