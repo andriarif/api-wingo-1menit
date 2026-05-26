a551b31901b02c0db340526314b15b6ad3ba96a8
andriarif <55911915+andriarif@users.noreply.github.com>
2026-05-26 17:46:18 +0700

    Fix formatting and ensure proper response handling

--- api/round.js
+++ api/round.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
     "https://didihub.com/api/main/lottery/rounds?page=1&count=20&type=3",
     {
       headers: {
-        Accept: "application/json, text/plain, */*",
-        user_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Nzk4NjMyOTAzMjYsInBheWxvYWQiOiJJL2NTdW5DR25mcFR3MkNoay9YeVJjOEpKV2xmRGlOd2Q0T0JTOWRYaklWODZVMCs4cmFZaVZqemlWM0Nxc1EyIn0.h-BkmTlY4H-QwuvjcHc9vgjXT1rwhHF25qm1nkMdaVY"
+        "Accept": "application/json, text/plain, */*",
+        "user_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Nzk4NjMyOTAzMjYsInBheWxvYWQiOiJJL2NTdW5DR25mcFR3MkNoay9YeVJjOEpKV2xmRGlOd2Q0T0JTOWRYaklWODZVMCs4cmFZaVZqemlWM0Nxc1EyIn0.h-BkmTlY4H-QwuvjcHc9vgjXT1rwhHF25qm1nkMdaVY"
       }
     }
   );
